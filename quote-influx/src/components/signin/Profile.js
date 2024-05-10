02dd56bce8b95750d6263f080329878a0b957d47
weihanghong <weihanghong2001@gmail.com>
2024-05-09 21:13:42 -0400

    profile page

--- quote-influx/src/components/signin/Profile.js
+++ quote-influx/src/components/signin/Profile.js
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import styles from './Profile.module.css';
 
 const Profile = ({username, setUsername}) => {
     const navigate = useNavigate();
@@ -21,12 +22,31 @@ const Profile = ({username, setUsername}) => {
     }
 
     return(
-        <div>
-            my profile
-            <div>
-                user: {username}
+        <div id={styles.profileContainer}>
+            <div id={styles.heading}>
+                Account Information
             </div>
-            <button onClick={handleLogOut}>Log out</button>
+            <div id={styles.infosContainer}>
+                <div className={styles.infoContainer}>
+                    <div className={styles.personalInfo}>
+                        Username:&nbsp;{username}
+                    </div>
+                    <div className={styles.edit}>
+                        edit
+                    </div>
+                </div>
+                <div className={styles.infoContainer}>
+                    <div className={styles.personalInfo}>
+                        Password:&nbsp;&nbsp;********
+                    </div>
+                    <div className={styles.edit}>
+                        edit
+                    </div>
+                </div>
+            </div>
+            <div id={styles.logoutButton}>
+                <button onClick={handleLogOut}>Log out</button>
+            </div>
         </div>
     );
 }
